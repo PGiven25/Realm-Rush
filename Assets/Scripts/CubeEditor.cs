6e966af86ad4445341c35d48cac22b0542ec5c4e
DESKTOP-RC008RD\Patrick <patrick.given25@gmail.com>
2018-09-17 18:24:40 -0400

    Cleaned code a bit (removed * gridSize)

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -24,18 +24,18 @@ public class CubeEditor : MonoBehaviour {
     private void SnapToGrid()
     {
         int gridSize = waypoint.GetGridSize();
-        transform.position = new Vector3(waypoint.GetGridPos().x, 
+        transform.position = new Vector3(waypoint.GetGridPos().x * gridSize, 
                                          0f,
-                                         waypoint.GetGridPos().y);
+                                         waypoint.GetGridPos().y * gridSize);
     }
 
     private void UpdateLabel()
     {
         int gridSize = waypoint.GetGridSize();
         TextMesh textMesh = GetComponentInChildren<TextMesh>();
-        string LabelText = waypoint.GetGridPos().x / gridSize + 
+        string LabelText = waypoint.GetGridPos().x + 
                             "," + 
-                            waypoint.GetGridPos().y / gridSize;
+                            waypoint.GetGridPos().y;
         textMesh.text = LabelText;
         gameObject.name = LabelText;
     }
