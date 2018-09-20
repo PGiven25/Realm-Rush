ca7d77f1ecdcece16e65a12deb60df2a43cee36e
DESKTOP-RC008RD\Patrick <patrick.given25@gmail.com>
2018-09-20 14:34:48 -0400

    Added basic HP for base

---
+++ Assets/Scripts/HealthBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBase : MonoBehaviour {
+    [SerializeField] int baseHP = 10;
+    [SerializeField] int healthDecrease = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        baseHP = baseHP - healthDecrease;
+    }
+}
